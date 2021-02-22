f3cbf9de029bfdbde20e7df4abd88638ac568a7c
Ali <alibaniasad1999@yahoo.com>
2021-02-22 14:52:15 +0330

    Add Quad Parameter from Abeshtan thesis

---
+++ Code/QuadParameter.m
@@ -0,0 +1,33 @@
+% Distance between rotor center to quad center
+d_cg = 0.295;
+% Distance between D point to quad center
+h_cg = 0.0078;
+% Thrust factor
+b = 1.10794e-5;
+% Rotor torque drag factor
+d = 3.2e-6;
+% Total mass
+m_tot = 1.774;
+% Quad Moment of inertia respect to axix X Body
+J_xx = 0.0297598;
+% Quad Moment of inertia respect to axix Y Body
+J_yy = 0.0298565;
+% Quad Moment of inertia respect to axix Y Body
+J_zz = 0.058510;
+% Quad blade Moment
+J_R = 4.439e-5;
+% Mass on X axis bearings
+m1 = 1.272;
+% Mass on Y axis bearings
+m2 = 1.074;
+% Mass on Z axis bearings
+m3 = 1.693;
+% Bearings radius in X axix
+r_x = 0.01;
+% Bearings radius in Y axix
+r_y = 0.01;
+% Bearings radius in Z axix
+r_z = 0.025;
+
+
+
