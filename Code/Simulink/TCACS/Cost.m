6088d476ed809c0a2e79e4c4f28a208d1435cc83
unknown <alibaniasad1999@yahoo.com>
2021-03-18 18:53:59 +0330

    litte change to be All update

--- Code/Simulink/TCACS/Cost.m
+++ Code/Simulink/TCACS/Cost.m
@@ -1,13 +1,11 @@
 function JCost = Cost(G)
 % assign G to workspace
 assignin('base', 'G', G);
-try
+
     % Run Simulink
     sim('MBK.slx');
     JCost = RefrenceError' * RefrenceError;
-catch
-        JCost = 1e5;
-end
+
 Iteration = evalin('base', 'Iteration');
 Iteration = Iteration + 1;
 assignin('base', 'Iteration', Iteration);
