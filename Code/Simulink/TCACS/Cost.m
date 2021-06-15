c033ccfc009e463b86d34a6bcafb354ceec4bc90
Ali <alibaniasad1999@yahoo.com>
2021-06-15 22:32:44 +0430

    change simulimk name

--- Code/Simulink/TCACS/Cost.m
+++ Code/Simulink/TCACS/Cost.m
@@ -4,7 +4,7 @@ assignin('base', 'Q', Q);
     % Run Simulink
     try
         RefrenceError = 0;
-        sim('MBK.slx');
+        sim('MBKwithLQR.slx');
         VCost = RefrenceError(:, 1)' * RefrenceError(:, 1);
         XCost = RefrenceError(:, 2)' * RefrenceError(:, 2);
         JCost = VCost + XCost;
