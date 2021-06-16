551c420999387b678e863c7da87abc9464158463
Ali <alibaniasad1999@yahoo.com>
2021-06-16 11:41:01 +0430

    fix  spacing

--- Code/Simulink/TCACS/Cost.m
+++ Code/Simulink/TCACS/Cost.m
@@ -1,17 +1,18 @@
 function JCost = Cost(Q)
+warning off all;
+close all;
 % assign G to workspace
 assignin('base', 'Q', Q);
-    % Run Simulink
-    try
-        RefrenceError = 0;
-        sim('MBKwithLQR.slx');
-        VCost = RefrenceError(:, 1)' * RefrenceError(:, 1);
-        XCost = RefrenceError(:, 2)' * RefrenceError(:, 2);
-        JCost = VCost + XCost;
-    catch
-        JCost = 1e5;
-    end
-
+% Run Simulink
+try
+    RefrenceError = 0;
+	sim('MBKwithLQR.slx');
+	VCost = RefrenceError(:, 1)' * RefrenceError(:, 1);
+	XCost = RefrenceError(:, 2)' * RefrenceError(:, 2);
+	JCost = VCost + XCost;
+catch
+	JCost = 1e5;
+end
 Iteration = evalin('base', 'Iteration');
 Iteration = Iteration + 1;
 assignin('base', 'Iteration', Iteration);
