a4274c9b6466961b58fa9ec04909c2c97e0fc9c4
Ali <alibaniasad1999@yahoo.com>
2021-06-19 00:15:32 +0430

    change value

--- Code/Simulink/TCACS/Finder.m
+++ Code/Simulink/TCACS/Finder.m
@@ -3,8 +3,8 @@ close all;
 clear
 clc
 clear
-Iteration = 0;
-MaxFE = 1000;
+Iteration =   0;
+MaxFE     = 100;
 a = [ 1  ,  1];
 b = [100, 100];
 [XOpt, FOpt, nFE] = TCACS('Cost', [a; b], MaxFE, 1e-4);
