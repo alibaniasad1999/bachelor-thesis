af4f8342d447256434a7bdf27be9348ca2a373c2
Ali <alibaniasad1999@yahoo.com>
2021-06-19 15:23:45 +0430

    add sym

--- Code/Simulink/Quadcopter/QuadSpaceState.m
+++ Code/Simulink/Quadcopter/QuadSpaceState.m
@@ -1,6 +1,13 @@
 %%% Quadcopter spacestate %%%
 % load data %
 QuadConstants;
+%{
+A1 = sym('A1', 'real');
+A3 = sym('A3', 'real');
+B1 = sym('B1', 'real');
+B3 = sym('B3', 'real');
+C2 = sym('C2', 'real');
+%}
 % dot(x) = Ax + Bu, u = omega^2 matrix
 A = [0  0  0 1 0 0  ;
      0  0  0 0 1 0  ;
