1fe30672f1f7f7a58a96a2a7ff301bcc76e9220c
Ali <alibaniasad1999@yahoo.com>
2021-08-09 21:20:11 +0430

    fix for SISO

--- Code/Ode45Simulation/SISO-Model/LQDG/SISO_Quadcopter_system.m
+++ Code/Ode45Simulation/SISO-Model/LQDG/SISO_Quadcopter_system.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%% Quadcopter system %%%%%%%%%%%%
-function [A, Bu1, Bu2, Bu3] = Quadcopter_system(X)
+function [Au1, Au2, Au3, Bu1, Bu2, Bu3] = SISO_Quadcopter_system(X)
 
 QuadConstants;
 
@@ -7,79 +7,45 @@ QuadConstants;
 
 x1     = X(1);
 x2     = X(2);
-x4     = X(4);
-x5     = X(5);
-x6     = X(6);
 
 %%%%%%%%% Linearization %%%%%%%%%
 
 %%%%% A matrix %%%%%
-A11    = x5 * cos(x1) * tan(x2)   - x6 * sin(x1) * tan(x2); %da1dx1
-A12    = x5 * sin(x1) / cos(x2)^2 + x6 * cos(x1) / cos(x2)^2; %da1dx2
-A13    = 0; %da1dx3
+A11    = 0; %da1dx1
 A14    = 1; %da1dx4
-A15    = sin(x1) * tan(x2); %da1dx5
-A16    = cos(x1) * tan(x2); %da1dx6
-A21    = -x5 * sin(x1) -x6 * cos(x1); %da2dx1
 A22    = 0; %da2dx2
-A23    = 0; %da2dx3
-A24    = 0; %da2dx4
-A25    =  cos(x1); %da2dx5
-A26    = -sin(x1); %da2dx6
-A31    = (x5 * cos(x1) - x6 * sin(x1)) * sec(x2);
-A32    = (x5 * sin(x1) + x6 * cos(x1)) * sec(x2) * tan(x2);
-A33    = 0;
-A34    = 0;
-A35    = sin(x1) * sec(x2);
-A36    = cos(x1) * sec(x2);
-A41    =  A1 * cos(x2) * cos(x1);
-A42    = -A2 * sin(x1) * sin(x2);
-A43    = 0;
-A44    = 0;
-A45    = A2 * x6;
-A46    = 0;
-A51    = 0;
-A52    = B1 * cos(x2);
-A53    = 0;
-A54    = B2 * x6;
-A55    = 0;
-A56    = B2 * x6;
-A61    = 0;
-A62    = 0;
-A63    = 0;
-A64    = C1 * x5;
-A65    = C1 * x4;
-A66    = 0;
+A25    = 1; %da2dx5
+A33    = 0; %da3dx3
+A36    = 1; %da3dx6
+A41    = A1 * cos(x1); %da4dx1
+A44    = 0; %da4dx4
+A52    = B1 * cos(x2); %da5dx2
+A55    = 0; %da5dx5
+A63    = 0; %da6dx3
+A66    = 0; %da6dx6
+%%%%%%%%%%%%%% A1x1 A2x2 A3x3 B1u1 + B2u2 + B3u3 %%%%%%%%%%%%%%
+% roll
+Au1    = [A11, A14 ;
+          A41, A44];
+% pitch
+Au2    = [A22, A25 ;
+          A52, A55];
+
+% yaw
+Au3    = [A33, A36 ;
+          A63, A66];
 %%%%% B matrix(B1, B2, B3) %%%%%
 %%% about u1, u2, u3 %%%
 % u1 = omega_2^2 - omega_4^2
 % u2 = omega_1^2 - omega_3^2
 % u1 = omega_1^2 - omega_2^2 + omega_3^2 - omega_4^2
-Bu1 = [0 ;
-      0 ;
-      0 ;
-      A3;
-      0 ;
-      0];
+Bu1     = [0  ;
+           A3];
   
-Bu2 = [0 ;
-      0 ;
-      0 ;
-      0 ;
-      B3;
-      0];
+Bu2     = [0  ;
+           B3];
   
-Bu3 = [0  ;
-      0  ;
-      0  ;
-      0  ;
-      0  ;
-      C2];
-%%%%%%%%%%%%%% Ax B1u1 + B2u2 + B3u3 %%%%%%%%%%%%%%
-A      = [A11 A12 A13 A14 A15 A16 ;
-          A21 A22 A23 A24 A25 A26 ;
-          A31 A32 A33 A34 A35 A36 ;
-          A41 A42 A43 A44 A45 A46 ;
-          A51 A52 A53 A54 A55 A56 ;
-          A61 A62 A63 A64 A65 A66];
+Bu3    = [0  ;
+          C2];
+
 end
--- Code/Ode45Simulation/SISO-Model/LQDG/linear_LQDG.m
+++ Code/Ode45Simulation/SISO-Model/LQDG/linear_LQDG.m
@@ -1,23 +1,39 @@
 clear
 clc
 
-global A B1 B2 B3 Q n R1_inv R2_inv U_1 U_2 U_3 U_t
+global A1 A2 A3 B1 B2 B3 n R1_inv R2_inv U_1 U_2 U_3 U_t
 U_1   = []; % save data u1
 U_2   = []; % save data u2
 U_3   = []; % save data u3
 U_t   = []; % time for u saver
 global S1_1 S2_1 S1_2 S2_2 S1_3 S2_3
-global u_saver t_u counter
 
-[A, B1, B2, B3] = Quadcopter_system(zeros(6, 1)); % Space State
+[A1, A2, A3, B1, B2, B3] = SISO_Quadcopter_system(zeros(6, 1)); % Space State
 
-q_v = [10
-       10 
-       10
-       10
-       10
+q_v1   = [10
+          10];
+Q1     = diag(q_v1);
+
+q_v2   = [10
+          10];
+Q2     = diag(q_v2);
+
+q_v3   = [1000000000
+          1000000000];
+Q3     = diag(q_v3);
+
+h_v1 = [10
        10];
-Q      = diag(q_v);
+H1	 = diag(h_v1);
+
+h_v2 = [10
+       10];
+H2	 = diag(h_v2);
+
+h_v3 = [1000000000
+        1000000000];
+H3	 = diag(h_v3);
+
 R1     = eye(1);
 R1_inv = R1^-1;
 R2     = 2 * eye(1);
@@ -34,52 +50,45 @@ S2_2   = B2 * R2_inv * B2';
 S1_3   = B3 * R1_inv * B3';
 S2_3   = B3 * R2_inv * B3';
 
-h_v = [10
-       10 
-       10
-       10
-       10
-       10];
-H	   = diag(h_v);
-
 tf     = 15; % final time
 
-k0	= [H;H];
-n	= 6;
-k0	= reshape(k0,2 * n^2,1);
+n	= 2;
 
-% initial variable 
+k01	= [H1;H1];
+k01	= reshape(k01,2 * n^2,1);
 
-counter = ones(1);
-u_saver = zeros(4, 1);
-t_u = zeros(1);
+k02	= [H2;H2];
+k02	= reshape(k02,2 * n^2,1);
+
+k03	= [H3;H3];
+k03	= reshape(k03,2 * n^2,1);
 
 global t_k_1 k_arr_1
 % u1 
-[t_k_1,k_arr_1] = ode45(@diff_eq_Riccati_u1,[tf,0],k0);
+[t_k_1,k_arr_1] = ode45(@diff_eq_Riccati_u1,[tf,0],k01);
 
 global t_k_2 k_arr_2
 % u2 
-[t_k_2,k_arr_2] = ode45(@diff_eq_Riccati_u2,[tf,0],k0);
+[t_k_2,k_arr_2] = ode45(@diff_eq_Riccati_u2,[tf,0],k02);
 
 global t_k_3 k_arr_3
 % u3 
-[t_k_3,k_arr_3] = ode45(@diff_eq_Riccati_u3,[tf,0],k0);
+[t_k_3,k_arr_3] = ode45(@diff_eq_Riccati_u3,[tf,0],k03);
 
 % u4 = omega_1^2 + omega_2^2 + omega_3^2 + omega_4^2 Assume 4*2000rpm
 
 x0 = [deg2rad(30) ; % roll
       deg2rad(45) ; % pitch
-      deg2rad(00) ; % yaw
+      deg2rad(30) ; % yaw
       deg2rad(60) ; % p
       deg2rad(50) ; % q
-      deg2rad(00)]; % r
+      deg2rad(30)]; % r
  
 % System Simulation
 [t,x] = ode45(@diff_eq_states,[0,tf],x0);
 
 % control effort
-U_4 = ones(1, length(U_1)) * 4 * rpm2rad_sec(4000)^2;
+U_4 = ones(1, length(U_1)) * 4 * rpm2rad_sec(2000)^2;
 
 % all U in one
 U = [U_1; U_2; U_3; U_4];
@@ -99,17 +108,20 @@ legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
     'interpreter', 'latex')
 xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
 ylabel('$System~State$', 'interpreter', 'latex');
-% figure(102)
-% plot(t_u, u_saver);
-% legend('$\omega_1$', '$\omega_2$', '$\omega_3$','$\omega_4$',...
-%     'interpreter', 'latex')
-% xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
-% ylabel('$Rad/{\sec}$', 'interpreter', 'latex');
+
+figure(102)
+plot(linspace(0, tf, length(omega)), omega);
+legend('$\omega_1$', '$\omega_2$', '$\omega_3$','$\omega_4$',...
+    'interpreter', 'latex')
+xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
+ylabel('$Rad/{\sec}$', 'interpreter', 'latex');
 
 %% Functions %%
 % u1
 function d = diff_eq_Riccati_u1(~,k)
-global A Q n S1_1 S2_1
+global A1 Q1 n S1_1 S2_1
+A  = A1;
+Q  = Q1;
 S1 = S1_1;
 S2 = S2_1;
 k	= reshape(k,2 * n,n);
@@ -122,7 +134,9 @@ end
 
 % u2
 function d = diff_eq_Riccati_u2(~,k)
-global A Q n S1_2 S2_2
+global A2 Q2 n S1_2 S2_2
+A  = A2;
+Q  = Q2;
 S1 = S1_2;
 S2 = S2_2;
 k	= reshape(k,2 * n,n);
@@ -135,7 +149,9 @@ end
 
 % u3
 function d = diff_eq_Riccati_u3(~,k)
-global A Q n S1_3 S2_3
+global A3 Q3 n S1_3 S2_3
+A  = A3;
+Q  = Q3;
 S1 = S1_3;
 S2 = S2_3;
 k	= reshape(k,2 * n,n);
@@ -147,10 +163,10 @@ d	= reshape([k1dot;k2dot],2 * n^2,1);
 end
 
 function d = diff_eq_states(t,x)
-global A n 
+global A1 A2 A3 n 
 % global u_saver counter t_u
 global t_k_1 k_arr_1 t_k_2 k_arr_2 t_k_3 k_arr_3 
-global S1_1 S2_1 S1_2 S2_2 S1_3 S2_3 R1_inv R2_inv
+global R1_inv R2_inv
 global U_1 U_2 U_3 U_t B1 B2 B3
 
 k_t_1	= interp1(t_k_1, k_arr_1, t);
@@ -170,18 +186,29 @@ k2_2 = k_2(n+1: end, :);
 k1_3 = k_3(1:n, :);
 k2_3 = k_3(n+1: end, :);
 
-u1	 = (-R1_inv*B1'*k1_1*x -R2_inv*B1'*k2_1) * x;
-u2	 = (-R1_inv*B2'*k1_2*x -R2_inv*B2'*k2_2) * x;
-u3   = (-R1_inv*B3'*k1_3*x -R2_inv*B3'*k2_3) * x;
+x1 = [x(1); x(4)];
+x2 = [x(2); x(5)];
+x3 = [x(3); x(6)];
+
+u1	 = (-R1_inv*B1'*k1_1 -R2_inv*B1'*k2_1) * x1;
+u2	 = (-R1_inv*B2'*k1_2 -R2_inv*B2'*k2_2) * x2;
+u3   = (-R1_inv*B3'*k1_3 -R2_inv*B3'*k2_3) * x3;
 % save data
 U_1(end+1) = u1;
 U_2(end+1) = u2;
 U_3(end+1) = u3;
 U_t(end+1) = t ;
 
-d	= (A - S1_1 * k1_1 - S2_1 * k2_1 ...
-         - S1_2 * k1_2 - S2_2 * k2_2 ...
-         - S1_3 * k1_3 - S2_3 * k2_3) * x;
+d1 = A1 * x1 + B1 * u1;
+d2 = A2 * x2 + B2 * u2;
+d3 = A3 * x3 + B3 * u3;
+
+d	= [d1(1) ;
+       d2(1) ;
+       d3(1) ;
+       d1(2) ;
+       d2(2) ;
+       d3(2)];
 % u = R1_inv * B' * k1 * x;
 % t_u(counter) = t;
 % u_saver(:, counter) = u;
