4c6780685c41f23646627bc5174079de7a6bc894
Ali <alibaniasad1999@yahoo.com>
2021-08-09 21:07:00 +0430

    fix for SISO LQR

--- Code/Ode45Simulation/SISO-Model/LQR/linear_LQR.m
+++ Code/Ode45Simulation/SISO-Model/LQR/linear_LQR.m
@@ -1,49 +1,65 @@
 clear
 clc
 
-global A B1 B2 B3 Q n R_inv U_1 U_2 U_3 U_t
+global A1 A2 A3 B1 B2 B3 Q1 Q2 Q3 n R_inv U_1 U_2 U_3 U_t
 U_1   = []; % save data u1
 U_2   = []; % save data u2
 U_3   = []; % save data u3
 U_t   = []; % time for u saver
 
-[A, B1, B2, B3] = Quadcopter_system(zeros(6, 1)); % Space State
+[A1, A2, A3, B1, B2, B3] = SISO_Quadcopter_system(zeros(6, 1)); % Space State
 
-q_v = [10
-       10 
-       10000000
-       10
-       10
-       10000000];
-Q      = diag(q_v);
+q_v1   = [10
+          10];
+Q1     = diag(q_v1);
+
+q_v2   = [10
+          10];
+Q2     = diag(q_v2);
+
+q_v3   = [1000000000
+          1000000000];
+Q3     = diag(q_v3);
+
 R      = 5;
 R_inv  = R^-1;
 
-h_v = [10
-       10 
-       10000000
-       10
-       10
-       10000000];
-H	 = diag(h_v);
+h_v1 = [10
+       10];
+H1	 = diag(h_v1);
+
+h_v2 = [10
+       10];
+H2	 = diag(h_v2);
+
+h_v3 = [1000000000
+        1000000000];
+H3	 = diag(h_v3);
 
 tf   = 20; % final time
 
-k0	= H;
-n	= 6;
-k0	= reshape(k0,n^2,1);
+n	= 2;
+
+k01	= H1;
+k01	= reshape(k01,n^2,1);
+
+k02	= H2;
+k02	= reshape(k02,n^2,1);
+
+k03	= H3;
+k03	= reshape(k03,n^2,1);
 
 global t_k_1 k_arr_1
 % u1 omega_2^2 - omega_4^2
-[t_k_1,k_arr_1] = ode45(@diff_eq_Riccati_u1,[tf,0],k0);
+[t_k_1,k_arr_1] = ode45(@diff_eq_Riccati_u1,[tf,0],k01);
 
 global t_k_2 k_arr_2
 % u2 omega_1^2 - omega_3^2
-[t_k_2,k_arr_2] = ode45(@diff_eq_Riccati_u2,[tf,0],k0);
+[t_k_2,k_arr_2] = ode45(@diff_eq_Riccati_u2,[tf,0],k02);
 
 global t_k_3 k_arr_3
 % u3 omega_1^2 - omega_2^2 + omega_3^2 - omega_4^2
-[t_k_3,k_arr_3] = ode45(@diff_eq_Riccati_u3,[tf,0],k0);
+[t_k_3,k_arr_3] = ode45(@diff_eq_Riccati_u3,[tf,0],k03);
 
 % u4 = omega_1^2 + omega_2^2 + omega_3^2 + omega_4^2 Assume 4*2000rpm
 
@@ -78,44 +94,50 @@ legend('$\phi$', '$\theta$', '$\psi$','$p$','$q$','$r$',...
     'interpreter', 'latex')
 xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
 ylabel('$System~State$', 'interpreter', 'latex');
-% figure(102)
-% plot(t_u, u_saver);
-% legend('$\omega_1$', '$\omega_2$', '$\omega_3$','$\omega_4$',...
-%     'interpreter', 'latex')
-% xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
-% ylabel('$Rad/{\sec}$', 'interpreter', 'latex');
+figure(102)
+plot(linspace(0, tf, length(omega)), omega);
+legend('$\omega_1$', '$\omega_2$', '$\omega_3$','$\omega_4$',...
+    'interpreter', 'latex')
+xlabel('$Time_{(\sec)}$', 'interpreter', 'latex');
+ylabel('$Rad/{\sec}$', 'interpreter', 'latex');
 
 %% Functions %%
 % u1
 function d = diff_eq_Riccati_u1(~,k)
-global A B1 Q n R_inv
-k	= reshape(k,n,n);
-B = B1;
+global A1 B1 Q1 n R_inv
+Q    = Q1;
+A    = A1;
+k    = reshape(k,n,n);
+B    = B1;
 kdot = -k*A - A'*k - Q + k*B*R_inv*B'*k;
-d	= reshape(kdot, n^2, 1);
+d	 = reshape(kdot, n^2, 1);
 end
 
 % u2
 function d = diff_eq_Riccati_u2(~,k)
-global A B2 Q n R_inv
-k	= reshape(k,n,n);
-B = B2;
+global A2 B2 Q2 n R_inv
+Q    = Q2;
+A    = A2;
+k  	 = reshape(k,n,n);
+B    = B2;
 kdot = -k*A - A'*k - Q + k*B*R_inv*B'*k;
-d	= reshape(kdot, n^2, 1);
+d	 = reshape(kdot, n^2, 1);
 end
 
 % u3
 function d = diff_eq_Riccati_u3(~,k)
-global A B3 Q n R_inv
-k	= reshape(k,n,n);
-B = B3;
+global A3 B3 Q3 n R_inv
+Q    = Q3;
+A    = A3;
+k    = reshape(k,n,n);
+B    = B3;
 kdot = -k*A - A'*k - Q + k*B*R_inv*B'*k;
-d	= reshape(kdot, n^2, 1);
+d	 = reshape(kdot, n^2, 1);
 end
 
 % System simulation
 function d = diff_eq_states(t,x)
-global A B1 B2 B3 n R_inv
+global A1 A2 A3 B1 B2 B3 n R_inv
 global U_1 U_2 U_3 U_t
 global t_k_1 k_arr_1 t_k_2 k_arr_2 t_k_3 k_arr_3 
 
@@ -127,16 +149,29 @@ k_1	= reshape(k_t_1,n,n);
 k_2	= reshape(k_t_2,n,n);
 k_3	= reshape(k_t_3,n,n);
 
-u1	  = -R_inv*B1'*k_1*x;
-u2	  = -R_inv*B2'*k_2*x;
-u3	  = -R_inv*B3'*k_3*x;
+x1 = [x(1); x(4)];
+x2 = [x(2); x(5)];
+x3 = [x(3); x(6)];
+
+u1	  = -R_inv*B1'*k_1*x1;
+u2	  = -R_inv*B2'*k_2*x2;
+u3	  = -R_inv*B3'*k_3*x3;
 % save data
 U_1(end+1) = u1;
 U_2(end+1) = u2;
 U_3(end+1) = u3;
 U_t(end+1) = t ;
 
-d	= A * x + B1 * u1 + B2 * u2 + B3 * u3;
+d1 = A1 * x1 + B1 * u1;
+d2 = A2 * x2 + B2 * u2;
+d3 = A3 * x3 + B3 * u3;
+
+d	= [d1(1) ;
+       d2(1) ;
+       d3(1) ;
+       d1(2) ;
+       d2(2) ;
+       d3(2)];
 end
 
 % rpm to rad/sec
