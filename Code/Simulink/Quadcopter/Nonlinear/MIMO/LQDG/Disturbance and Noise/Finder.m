ff9e2bfc0dfac45484375230ac8bb2fc061a1693
unknown <alibaniasad1999@yahoo.com>
2021-10-19 08:58:19 +0330

    add TCACS for noise

---
+++ Code/Simulink/Quadcopter/Nonlinear/MIMO/LQDG/Disturbance and Noise/Finder.m
@@ -0,0 +1,20 @@
+warning off all;
+close all;
+clear
+clc
+clear
+Iteration =     0;
+MaxFE     = 15000;
+%     roll and pitch yaw  p and q   r    R2
+min = [     0,        0,     0,     0,   -2];
+max = [     9,        9,     9,     9,    6];
+tic
+[XOpt, FOpt, nFE] = TCACS('Cost', [min; max], MaxFE, 1e-4);
+toc
+savedata = {XOpt, FOpt, nFE};
+now_time = string(datetime('now'));
+str_now_time = now_time{1};
+str_now_time(12) = '-';
+str_now_time(15) = '-';
+str_now_time(18) = '-';
+save(append(str_now_time, '-', string(nFE)), 'savedata')
