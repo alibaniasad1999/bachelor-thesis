8e7fe6989f6d13d8c007d649f840eca09cbccb09
Ali <alibaniasad1999@yahoo.com>
2021-11-28 19:47:39 +0330

    fix data from parameter estimation

--- Code/Simulink/Quadcopter/Lab/RCP/PID/Pitch/Parameter_estimation/QuadParameter.m
+++ Code/Simulink/Quadcopter/Lab/RCP/PID/Pitch/Parameter_estimation/QuadParameter.m
@@ -1,17 +1,22 @@
 % Distance between rotor center to quad center (m)
-d_cg = 0.2;
+% d_cg = 0.2;
+d_cg = 0.207098413843917;
 % Distance between D point to quad center (m)
-h_cg = 0.02;
+% h_cg = 0.02;
+h_cg = 0.00425905946163903;
 % Thrust factor (non dimensional)
-b = 3.13e-5;
+% b = 3.13e-5;
+b = 4.06931591902712e-05;
 % Rotor torque drag factor (non dimensional)
 d = 3.2e-6;
 % Total mass (kg)
-m_tot = 0.638;
+% m_tot = 0.638;
+m_tot = 0.852034403932799;
 % Quad Moment of inertia respect to axix X Body (kg.m^2)
 J_xx = 0.02839;
 % Quad Moment of inertia respect to axix Y Body (kg.m^2)
-J_yy = 0.03066;
+% J_yy = 0.03066;
+J_yy = 0.00862342271204996;
 % Quad Moment of inertia respect to axix Y Body (kg.m^2)
 J_zz = 0.0439;
 % % Quad blade Moment (kg.m^2)
