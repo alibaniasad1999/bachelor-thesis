d03bcf39287d163f4800869f14af92bf72a5d4b7
unknown <alibaniasad1999@yahoo.com>
2021-11-29 18:00:34 +0330

    revert quad parameter and delete quad constanst

--- Code/Simulink/Quadcopter/Lab/RCP/PID/Pitch/Parameter_estimation/QuadParameter.m
+++ Code/Simulink/Quadcopter/Lab/RCP/PID/Pitch/Parameter_estimation/QuadParameter.m
@@ -1,45 +1,40 @@
 % Distance between rotor center to quad center (m)
-% d_cg = 0.2;
-d_cg = 0.207098413843917;
+d_cg = 0.295;
 % Distance between D point to quad center (m)
-% h_cg = 0.02;
-h_cg = 0.00425905946163903;
+h_cg = 0.0078;
 % Thrust factor (non dimensional)
-% b = 3.13e-5;
-b = 4.06931591902712e-05;
+b = 1.10794e-5;
 % Rotor torque drag factor (non dimensional)
 d = 3.2e-6;
 % Total mass (kg)
-% m_tot = 0.638;
-m_tot = 0.852034403932799;
+m_tot = 1.074;
 % Quad Moment of inertia respect to axix X Body (kg.m^2)
-J_xx = 0.02839;
+J_xx = 0.0297598;
 % Quad Moment of inertia respect to axix Y Body (kg.m^2)
-% J_yy = 0.03066;
-J_yy = 0.00862342271204996;
+J_yy = 0.0298565;
 % Quad Moment of inertia respect to axix Y Body (kg.m^2)
-J_zz = 0.0439;
-% % Quad blade Moment (kg.m^2)
-% J_R = 4.439e-5;
-% % Mass on X axis bearing (kg)
-% m1 = 1.272;
-% % Mass on Y axis bearing (kg)
-% m2 = 1.074;
-% % Mass on Z axis bearing (kg)
-% m3 = 1.693;
-% % Bearing radius in X axix (m)
-% r_x = 0.01;
-% % Bearing radius in Y axix (m)
-% r_y = 0.01;
-% % Bearing radius in Z axix (m)
-% r_z = 0.025;
-% % Static bearing friction (non dimensional)
-% miu_s = 0.003;
-% % Dynamic bearing friction (non dimensional)
-% miu_k = 0.002;
-% % Motor constant (non dimensional)
-% tau = 0.15;
-% % Delay of motor (sec)
-% T_d = 0.02;
-% % Gravity force (m/s^2)
+J_zz = 0.058510;
+% Quad blade Moment (kg.m^2)
+J_R = 4.439e-5;
+% Mass on X axis bearing (kg)
+m1 = 1.272;
+% Mass on Y axis bearing (kg)
+m2 = 1.074;
+% Mass on Z axis bearing (kg)
+m3 = 1.693;
+% Bearing radius in X axix (m)
+r_x = 0.01;
+% Bearing radius in Y axix (m)
+r_y = 0.01;
+% Bearing radius in Z axix (m)
+r_z = 0.025;
+% Static bearing friction (non dimensional)
+miu_s = 0.003;
+% Dynamic bearing friction (non dimensional)
+miu_k = 0.002;
+% Motor constant (non dimensional)
+tau = 0.15;
+% Delay of motor (sec)
+T_d = 0.02;
+% Gravity force (m/s^2)
 g = 9.81;
