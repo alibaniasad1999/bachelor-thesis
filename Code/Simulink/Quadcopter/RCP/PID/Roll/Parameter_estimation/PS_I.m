b6e47caf62db8ea0e2a702117a4629a8d06d8cfb
Ali <alibaniasad1999@yahoo.com>
2021-12-05 00:59:38 +0330

    done 2

--- Code/Simulink/Quadcopter/RCP/PID/Roll/Parameter_estimation/PS_I.m
+++ Code/Simulink/Quadcopter/RCP/PID/Roll/Parameter_estimation/PS_I.m
@@ -4,6 +4,6 @@ load_data;
 % saved data %
 load('Scenario_I.mat');
 sample_time = .02;
-time = 0:sample_time:15;
+time = 0:sample_time:14.6;
 time = time';
-AHRS_RCP  =  AHRS(251:end, :); % deg to rad
+AHRS_RCP  =  AHRS(271:end, :); % deg to rad
--- Code/Simulink/Quadcopter/RCP/PID/Roll_pitch/Parameter_estimation/PS_II.m
+++ Code/Simulink/Quadcopter/RCP/PID/Roll_pitch/Parameter_estimation/PS_II.m
@@ -4,6 +4,6 @@ load_data;
 % saved data %
 load('Scenario_II.mat');
 sample_time = .02;
-time = 0:sample_time:14.6;
+time = 0.4:sample_time:15;
 time = time';
 AHRS_RCP  =  AHRS(271:end, :); % deg to rad
