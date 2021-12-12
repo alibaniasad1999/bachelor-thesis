b93f1134098a28bae9dbda145eb02641d64d03f0
Ali <alibaniasad1999@yahoo.com>
2021-12-12 18:37:31 +0330

    add parameter estimation result

---
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Pitch/Parameter_estimation/PS_IV.m
@@ -0,0 +1,6 @@
+load('Scenario_IV.mat', 'Omega_S1', 'AHRS_rate_S1', 'AHRS_S1');
+Omega = Omega_S1(1, :);
+init = [AHRS_S1(1, 2), AHRS_rate_S1(1, 2)]*pi/180;
+time = 0:0.02:2;
+time = time';
+data = [AHRS_S1(261:261+100, 2), AHRS_rate_S1(261:261+100, 2)]*pi/180;
