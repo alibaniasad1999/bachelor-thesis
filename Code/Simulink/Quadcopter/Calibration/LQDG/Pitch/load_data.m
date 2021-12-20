54120f23c0badb4d1f79f5b7eeb1de7e9ce527e2
Ali <alibaniasad1999@yahoo.com>
2021-12-20 21:21:54 +0330

    add nothing just for push

--- Code/Simulink/Quadcopter/Calibration/LQDG/Pitch/load_data.m
+++ Code/Simulink/Quadcopter/Calibration/LQDG/Pitch/load_data.m
@@ -1,5 +1,5 @@
 % load data
 QuadConstants;
 % load('20-Dec-2021-01-16-27-noise_ite10005.mat');
-weighting_matrix =[11.9993    4.6516   11.5730    3.9309    0.1258];
+weighting_matrix =[12.5    4.6516   7.5730    3.9309    -1];
 untitled2;
