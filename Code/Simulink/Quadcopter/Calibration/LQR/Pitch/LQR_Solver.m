9ae767e217c55798cc068aeb8910d76783b1505d
Ali <alibaniasad1999@yahoo.com>
2021-12-21 09:58:04 +0330

    add my moment with name moment ali

--- Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_Solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_Solver.m
@@ -3,6 +3,8 @@ QuadConstants;
 A = [0 1;
     B1 0];
 B = [0; B3];
-Q = diag([100, 100]);
-R =1;
-[~, K, ~] = icare(A, B, Q, R);
+C = eye(2);
+D = zeros(2, 1);
+Q = diag([100000, 100]);
+R =0.000001;
+[~, K, ~] = icare(A, B, Q, R)
--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Pitch/LQR_Solver.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Pitch/LQR_Solver.m
@@ -3,6 +3,8 @@ QuadConstants;
 A = [0 1;
     B1 0];
 B = [0; B3];
-Q = diag([100, 100]);
-R =1;
-[~, K, ~] = icare(A, B, Q, R);
+C = eye(2);
+D = zeros(2, 1);
+Q = diag([10000, 100000]);
+R =0.00000001;
+[~, K, ~] = icare(A, B, Q, R)
