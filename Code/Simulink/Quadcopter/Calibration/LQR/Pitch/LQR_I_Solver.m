550071681da3683314b576be6a8d8749bef8e058
Ali <alibaniasad1999@yahoo.com>
2021-12-23 14:56:59 +0330

    there is problem with integrator in simulink arduino

--- Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_I_Solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_I_Solver.m
@@ -2,7 +2,7 @@
 QuadConstants;
 A = [0 1;
     B1 0];
-B = [0; B3];
+B = [0; 1];
 C = eye(2);
 D = zeros(2, 1);
 load('21-Dec-2021-20-04-30-noise_ite_Pitch_LQR1005.mat')
@@ -10,7 +10,6 @@ weighting_matrix = XOpt;
 Q1 = 10^(weighting_matrix(1));
 Q2 = 10^(weighting_matrix(2));
 R  = 10^weighting_matrix(3);
-I_gain = 10^weighting_matrix(4)-2;
-I_gain = 0;
+I_gain = 10^(weighting_matrix(4)-3);
 Q = diag([Q1, Q2]);
 [~, K, ~] = icare(A, B, Q, R);
