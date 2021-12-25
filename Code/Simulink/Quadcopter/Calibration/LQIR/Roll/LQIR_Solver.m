ce47416d3c062b16afc5b76b88936744f55883b9
Ali <alibaniasad1999@yahoo.com>
2021-12-25 14:48:29 +0330

    fix TCACS and add roll LQIR

--- Code/Simulink/Quadcopter/Calibration/LQIR/Roll/LQIR_Solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQIR/Roll/LQIR_Solver.m
@@ -7,11 +7,12 @@ D = zeros(2, 1);
 A_a = [A zeros(2) ;...
        C zeros(2)];
 B_a = [B; zeros(2, 1)];
+load('25-Dec-2021-14-36-13-noise_ite_Roll_LQIR1005.mat');
+weighting_matrix = XOpt;
 Q1 = 10^weighting_matrix(1);
 Q2 = 10^weighting_matrix(2);
 Q3 = 10^weighting_matrix(3);
 Q4 = 10^weighting_matrix(4);
 R  = 10^weighting_matrix(5);
-% I  = weighting_matrix(4)^10;
 Q = diag([Q1, Q2, Q3, Q4]);
 [~, K, ~] = icare(A_a, B_a, Q, R);
--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIR/Roll/TCACS/Finder.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIR/Roll/TCACS/Finder.m
@@ -4,7 +4,7 @@ clear
 clc
 clear
 Iteration =     0;
-MaxFE     = 10000;
+MaxFE     = 1000;
 %          Q_1     Q_2    Q_3     Q_4        R 
 min = [    -10,     -10,    -10,     -10,       -10];
 max = [     10,      10,     10,      10,        10];
@@ -17,4 +17,4 @@ str_now_time = now_time{1};
 str_now_time(12) = '-';
 str_now_time(15) = '-';
 str_now_time(18) = '-';
-save(append(str_now_time, '-', 'noise_ite_Pitch_LQIR_no_i', string(nFE)), 'XOpt', 'FOpt', 'nFE');
+save(append(str_now_time, '-', 'noise_ite_Roll_LQIR', string(nFE)), 'XOpt', 'FOpt', 'nFE');
