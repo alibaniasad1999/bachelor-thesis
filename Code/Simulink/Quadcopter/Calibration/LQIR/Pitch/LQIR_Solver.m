ea98a80121914513c732dea9d1f98bc5ced51756
Ali <alibaniasad1999@yahoo.com>
2021-12-25 14:29:49 +0330

    fix model to 10 power

--- Code/Simulink/Quadcopter/Calibration/LQIR/Pitch/LQIR_Solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQIR/Pitch/LQIR_Solver.m
@@ -7,10 +7,12 @@ D = zeros(2, 1);
 A_a = [A zeros(2) ;...
        C zeros(2)];
 B_a = [B; zeros(2, 1)];
-Q1 = weighting_matrix(1)+2;
-Q2 = weighting_matrix(2);
-Q3 = weighting_matrix(3);
-Q4 = weighting_matrix(4);
-R  = weighting_matrix(5);
+load('22-Dec-2021-17-27-21-noise_ite_Pitch_LQIR_no_i1005.mat');
+weighting_matrix = XOpt;
+Q1 = 10^weighting_matrix(1);
+Q2 = 10^weighting_matrix(2);
+Q3 = 10^weighting_matrix(3);
+Q4 = 10^weighting_matrix(4);
+R  = 10^weighting_matrix(5);
 Q = diag([Q1, Q2, Q3, Q4]);
 [~, K, ~] = icare(A_a, B_a, Q, R);
