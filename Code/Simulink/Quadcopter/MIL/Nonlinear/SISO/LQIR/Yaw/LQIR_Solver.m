137dd1a5f63b17ec692ce5fd5172df328fd6ac8f
Ali <alibaniasad1999@yahoo.com>
2021-12-25 18:43:20 +0330

    fix function input

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIR/Yaw/LQIR_Solver.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIR/Yaw/LQIR_Solver.m
@@ -7,11 +7,11 @@ D = zeros(2, 1);
 A_a = [A zeros(2) ;...
        C zeros(2)];
 B_a = [B; zeros(2, 1)];
-Q1 = weighting_matrix(1);
-Q2 = weighting_matrix(2);
-Q3 = weighting_matrix(3);
-Q4 = weighting_matrix(4);
-R  = weighting_matrix(5);
+Q1 = 10^weighting_matrix(1);
+Q2 = 10^weighting_matrix(2);
+Q3 = 10^weighting_matrix(3);
+Q4 = 10^weighting_matrix(4);
+R  = 10^weighting_matrix(5);
 % I  = weighting_matrix(4)^10;
 Q = diag([Q1, 1, 1, .001]);
 [~, K, ~] = icare(A_a, B_a, Q, R);
