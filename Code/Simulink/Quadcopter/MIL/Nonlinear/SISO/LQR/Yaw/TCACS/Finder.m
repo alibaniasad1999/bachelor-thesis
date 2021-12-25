ad9771a26cd05f3863e19a87d5b17e53d3d48cb8
Ali <alibaniasad1999@yahoo.com>
2021-12-25 15:58:31 +0330

    add yaw lqr

---
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Yaw/TCACS/Finder.m
@@ -0,0 +1,20 @@
+warning off all;
+close all;
+clear
+clc
+clear
+Iteration =    0;
+MaxFE     = 1000;
+%          Q_1       Q_2        R 
+min = [    -6,       -6,       -6];
+max = [    3,         3,        3];
+tic
+[XOpt, FOpt, nFE] = TCACS('Cost', [min; max], MaxFE, 1e-4);
+toc
+savedata = {XOpt, FOpt, nFE};
+now_time = string(datetime('now'));
+str_now_time = now_time{1};
+str_now_time(12) = '-';
+str_now_time(15) = '-';
+str_now_time(18) = '-';
+save(append(str_now_time, '-', 'noise_Yaw_LQR_ite', string(nFE)), 'XOpt', 'FOpt', 'nFE');
