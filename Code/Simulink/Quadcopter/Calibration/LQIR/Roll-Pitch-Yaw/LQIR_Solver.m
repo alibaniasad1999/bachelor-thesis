4ebcbe136b29a1a2774a68ca3d4e9c4c9d417209
Ali <alibaniasad1999@yahoo.com>
2021-12-25 17:29:40 +0330

    add yaw lqr and add yaw in 3dof

---
+++ Code/Simulink/Quadcopter/Calibration/LQIR/Roll-Pitch-Yaw/LQIR_Solver.m
@@ -0,0 +1,35 @@
+QuadConstants;
+% roll
+A = [0 1;
+    A1 0];
+B = [0; 1];
+C = eye(2);
+D = zeros(2, 1);
+A_a = [A zeros(2) ;...
+       C zeros(2)];
+B_a = [B; zeros(2, 1)];
+load('25-Dec-2021-14-36-13-noise_ite_Roll_LQIR1005.mat');
+weighting_matrix = XOpt;
+Q1 = 10^weighting_matrix(1);
+Q2 = 10^weighting_matrix(2);
+Q3 = 10^weighting_matrix(3);
+Q4 = 10^weighting_matrix(4);
+R  = 10^weighting_matrix(5);
+Q = diag([Q1, Q2, Q3, Q4]);
+[~, K_roll, ~] = icare(A_a, B_a, Q, R);
+% pitch
+A = [0 1;
+    B1 0];
+B = [0; 1];
+A_a = [A zeros(2) ;...
+       C zeros(2)];
+B_a = [B; zeros(2, 1)];
+load('22-Dec-2021-17-27-21-noise_ite_Pitch_LQIR_no_i1005.mat');
+weighting_matrix = XOpt;
+Q1 = 10^weighting_matrix(1);
+Q2 = 10^weighting_matrix(2);
+Q3 = 10^weighting_matrix(3);
+Q4 = 10^weighting_matrix(4);
+R  = 10^weighting_matrix(5);
+Q = diag([Q1, Q2, Q3, Q4]);
+[~, K_pitch, ~] = icare(A_a, B_a, Q, R);
---
+++ Code/Simulink/Quadcopter/Calibration/LQIR/Roll-Pitch-Yaw/LQR_Solver.m
@@ -0,0 +1,13 @@
+QuadConstants;
+load('25-Dec-2021-15-57-24-noise_Yaw_LQR_ite1000.mat');
+A = [0 1;
+     0 0];
+B = [0; 1];
+C = eye(2);
+D = zeros(2, 1);
+weighting_matrix = XOpt;
+Q1 = 10^(weighting_matrix(1));
+Q2 = 10^(weighting_matrix(2));
+R  = 10^weighting_matrix(3);
+Q = diag([Q1, Q2]);
+[~, K, ~] = icare(A, B, Q, R);
