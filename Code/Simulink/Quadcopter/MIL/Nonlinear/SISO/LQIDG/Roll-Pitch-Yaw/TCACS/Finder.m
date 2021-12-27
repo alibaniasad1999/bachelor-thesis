4177393268ce0784a5b5e85f0b7d6a46c8eb4eeb
unknown <alibaniasad1999@yahoo.com>
2021-12-27 17:24:41 +0330

    fix delay and change Q range and add TCACS ans

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch-Yaw/TCACS/Finder.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch-Yaw/TCACS/Finder.m
@@ -7,8 +7,8 @@ Iteration =     0;
 MaxFE     = 1000;
 % roll
 %           Q1  Q2  Q3  Q4
-min_roll = [-6, -6, -6, -6];
-max_roll = [ 3,  3,  3,  3];
+min_roll = [-1, -1, -2, -2];
+max_roll = [ 1,  1,  1,  1];
 % pitch
 %            Q1  Q2  Q3  Q4
 % min_pitch = [-6, -6, -6, -6];
@@ -16,10 +16,10 @@ max_roll = [ 3,  3,  3,  3];
 % yaw
 %          Q1  Q2  Q3  Q4
 min_yaw = [-6, -6, -6, -6];
-max_yaw = [ 6,  6,  6,  6];
+max_yaw = [-3, -3, -3, -3];
 % R2
-min_R = -6;
-max_R =  6;
+min_R =  4;
+max_R =  7;
 min = [min_roll, min_yaw, min_R];
 max = [max_roll, max_yaw, max_R];
 tic
