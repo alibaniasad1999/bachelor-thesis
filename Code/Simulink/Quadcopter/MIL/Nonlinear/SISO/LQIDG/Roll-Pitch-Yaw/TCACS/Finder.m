c266656af7e03266c7daae1e2d91075059fad7c6
Ali <alibaniasad1999@yahoo.com>
2021-12-27 17:34:05 +0330

    make lowpass discret and chagne finder

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch-Yaw/TCACS/Finder.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch-Yaw/TCACS/Finder.m
@@ -11,8 +11,8 @@ min_roll = [-1, -1, -2, -2];
 max_roll = [ 1,  1,  1,  1];
 % pitch
 %            Q1  Q2  Q3  Q4
-% min_pitch = [-6, -6, -6, -6];
-% max_pitch = [ 6,  6,  6,  6];
+min_pitch = [-1, -1, -2, -2];
+max_pitch = [ 1,  1,  1,  1];
 % yaw
 %          Q1  Q2  Q3  Q4
 min_yaw = [-6, -6, -6, -6];
@@ -20,10 +20,10 @@ max_yaw = [-3, -3, -3, -3];
 % R2
 min_R =  4;
 max_R =  7;
-min = [min_roll, min_yaw, min_R];
-max = [max_roll, max_yaw, max_R];
+min = [min_roll, min_pitch, min_yaw, min_R];
+max = [max_roll, max_pitch, max_yaw, max_R];
 tic
-[XOpt, FOpt, nFE] = TCACS('Cost', [min; max], MaxFE, 2);
+[XOpt, FOpt, nFE] = TCACS('Cost', [min; max], MaxFE, 1e-4);
 toc
 savedata = {XOpt, FOpt, nFE};
 now_time = string(datetime('now'));
@@ -31,4 +31,4 @@ str_now_time = now_time{1};
 str_now_time(12) = '-';
 str_now_time(15) = '-';
 str_now_time(18) = '-';
-save(append(str_now_time, '-', 'noise_ite', string(nFE)), 'XOpt', 'FOpt', 'nFE');
+save(append(str_now_time, '-', 'LQIDG_3DOF', string(nFE)), 'XOpt', 'FOpt', 'nFE');
