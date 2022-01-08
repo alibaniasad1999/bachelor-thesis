c1716ac1dc15fd5a83c206fe4bbefe7c294ba79b
Ali <alibaniasad1999@yahoo.com>
2022-01-08 20:57:20 +0330

    ps_III

--- Code/Simulink/Quadcopter/RCP/OpenLoop/Roll-Pitch/parameter_estimation/QuadConstants.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Roll-Pitch/parameter_estimation/QuadConstants.m
@@ -17,5 +17,5 @@ C2 = (d) / (J_zz);
 C3 = (m3 * g * miu_k * r_z) / (J_zz);
 % omega for linearzation 2000 RPM
 Omega = 2000 / 60 * 2 * pi;
-roll_parameter_estimation_result;
-pitch_parameter_estiation_result;
+roll_parameter_estiation_result_II;
+pitch_parameter_estiation_result_II;
