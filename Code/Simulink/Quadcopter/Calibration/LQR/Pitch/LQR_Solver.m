c7212425685c43fad773b933b6d1d0e853ad5111
Ali <alibaniasad1999@yahoo.com>
2022-01-08 16:23:34 +0330

    new ps but have maby bug

--- Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_Solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQR/Pitch/LQR_Solver.m
@@ -1,10 +1,11 @@
+clc;clear;
 QuadConstants;
 A = [0 1;
     B1 0];
 B = [0; 1];
 C = eye(2);
 D = zeros(2, 1);
-load('23-Dec-2021-13-11-38-noise_ite_Pitch_LQR_no_i950.mat')
+load('26-Dec-2021-18-28-13-noise_ite_Pitch_LQR_no_i1400.mat')
 weighting_matrix = XOpt;
 Q1 = 10^(weighting_matrix(1));
 Q2 = 10^(weighting_matrix(2));
