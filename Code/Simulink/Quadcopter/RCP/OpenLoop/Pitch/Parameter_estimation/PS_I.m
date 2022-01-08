7c74d1643479ebfbdb2044c8b924d6749b60272d
Ali <alibaniasad1999@yahoo.com>
2022-01-08 17:31:07 +0330

    new ps

--- Code/Simulink/Quadcopter/RCP/OpenLoop/Pitch/Parameter_estimation/PS_I.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Pitch/Parameter_estimation/PS_I.m
@@ -2,5 +2,7 @@ load('Scenario_I.mat', 'Omega_S1', 'AHRS_rate_S1', 'AHRS_S1');
 Omega = Omega_S1(1, :);
 time = 0:.02:.5;
 time = time';
+AHRS_S1 = lowpass(AHRS_S1, 50, 1e3);
+AHRS_rate_S1 = lowpass(AHRS_rate_S1, 50, 1e3);
 data = [AHRS_S1(345:370, 2), AHRS_rate_S1(345:370, 2)]*pi/180;
 init = data(1, :);
