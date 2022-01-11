00d3df42f6248d7cd95a4dd031911fb83a4d84f6
Ali <alibaniasad1999@yahoo.com>
2022-01-11 16:41:51 +0330

    new perfect pitch controller and model

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Pitch/TCACS/QuadConstants.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Pitch/TCACS/QuadConstants.m
@@ -4,7 +4,7 @@ A1 = (h_cg * g * m_tot) / (m_tot * h_cg^2 + J_xx);
 A2 = (2*m_tot * h_cg^2 + J_yy - J_zz) / (m_tot * h_cg^2 + J_xx);
 A3 = (b * d_cg) / (m_tot * h_cg^2 + J_xx);
 A4 = (J_R) / (m_tot * h_cg^2 + J_xx);
-% A5 = (m1 * g * miu_k * r_x) / (m_tot * h_cg^2 + J_xx);
+A5 = (m1 * g * miu_k * r_x) / (m_tot * h_cg^2 + J_xx);
 % B Constants
 B1 = (h_cg * g * m_tot) / (m_tot * h_cg^2 + J_yy);
 B2 = (-2*m_tot * h_cg^2 - J_xx + J_zz) / (m_tot * h_cg^2 + J_yy);
