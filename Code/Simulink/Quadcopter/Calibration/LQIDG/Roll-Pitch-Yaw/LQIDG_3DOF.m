e1498fa1934eae2aa930174cf3ff3806ba345265
Ali <alibaniasad1999@yahoo.com>
2022-02-21 16:22:20 +0330

    fix equation but weighting matrix is bad

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/LQIDG_3DOF.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/LQIDG_3DOF.m
@@ -1,7 +1,8 @@
 % LQIDG Solver for 3DOF SISO
 clc;
 clear;
-load('27-Dec-2021-18-53-22-LQIDG_3DOF2500.mat');
+load('20-Feb-2022-21-07-59-LQIDG_3DOF4995.mat');
+weighting_matrix = XOpt;
 Roll_OpenLoop_LQIDG_solver;
 Pitch_OpenLoop_LQIDG_solver;
 Yaw_OpenLoop_LQIDG_solver;
