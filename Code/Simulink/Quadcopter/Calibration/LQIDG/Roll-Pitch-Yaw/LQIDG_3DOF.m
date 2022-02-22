f850ee81316ec8dbd9ecf18a847f618d1ff717c0
Ali <alibaniasad1999@yahoo.com>
2022-02-22 16:50:58 +0330

    fix model

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/LQIDG_3DOF.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/LQIDG_3DOF.m
@@ -1,8 +1,11 @@
 % LQIDG Solver for 3DOF SISO
 clc;
 clear;
-load('20-Feb-2022-21-07-59-LQIDG_3DOF4995.mat');
+load('11-Jan-2022-15-55-56-LQIDG_noise_ite1005.mat');
 weighting_matrix = XOpt;
 Roll_OpenLoop_LQIDG_solver;
 Pitch_OpenLoop_LQIDG_solver;
+load('21-Feb-2022-16-37-47-LQIDG_3DOF1005.mat');
+weighting_matrix = XOpt;
+% weighting_matrix(end) = weighting_matrix(end) + 1;
 Yaw_OpenLoop_LQIDG_solver;
