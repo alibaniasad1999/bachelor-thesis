6c7d2699cb1ce8bf7122aefe7c0fe4f7e33cd63c
Ali <alibaniasad1999@yahoo.com>
2022-03-28 22:44:37 +0430

    add image

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
@@ -1,4 +1,15 @@
 load('28-Mar-2022-21-13-33-noise_ite_Roll_LQR_no1000.mat');
 weighting_matrix = XOpt;
 out = sim('Quad_Nonlinear_Roll_LQR_Model');
-plot(out.roll_state)
+
+subplot(2,1,1);
+plot(out.tout, out.roll_state(:, 1))
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
+
+subplot(2,1,2);
+plot(out.tout, out.roll_state(:, 2))
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
+
+print('../../../../../../../../Figures/MIL/LQR/Roll/lqr_roll.png','-dpng','-r400');
