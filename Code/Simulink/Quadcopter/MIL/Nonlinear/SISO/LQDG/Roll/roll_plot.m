da364c011f76508e0ff3acaee02ceaffc75dde40
Ali <alibaniasad1999@yahoo.com>
2022-03-29 18:18:34 +0430

    beautifull fig

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQDG/Roll/roll_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQDG/Roll/roll_plot.m
@@ -3,12 +3,20 @@ weighting_matrix = XOpt;
 out = sim('Quad_Nonlinear_Roll_Model_LQDG');
 
 subplot(2,1,1);
-plot(out.tout, out.roll_state(:, 1), 'linewidth', 2);
+plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
+hold on;
+plot(out.tout, out.roll_state(:, 1), 'r', 'linewidth', 2);
+hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
+legend('Setpoint', 'LQDG Controller', 'Location','northeast');
 subplot(2,1,2);
-plot(out.tout, out.roll_state(:, 2), 'linewidth', 2);
+plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
+hold on
+plot(out.tout, out.roll_state(:, 2), 'r', 'linewidth', 2);
+hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
+legend('Setpoint', 'LQDG Controller', 'Location','northeast');
 
 print('../../../../../../../../Figures/MIL/LQDG/Roll/lqdg_roll.png','-dpng','-r400');
--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
@@ -3,12 +3,20 @@ weighting_matrix = XOpt;
 out = sim('Quad_Nonlinear_Roll_LQR_Model');
 
 subplot(2,1,1);
-plot(out.tout, out.roll_state(:, 1), 'linewidth', 2);
+plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
+hold on;
+plot(out.tout, out.roll_state(:, 1), 'r', 'linewidth', 2);
+hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
+legend('Setpoint', 'LQR Controller', 'Location','northeast');
 subplot(2,1,2);
-plot(out.tout, out.roll_state(:, 2), 'linewidth', 2);
+plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
+hold on
+plot(out.tout, out.roll_state(:, 2), 'r', 'linewidth', 2);
+hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
+legend('Setpoint', 'LQR Controller', 'Location','northeast');
 
 print('../../../../../../../../Figures/MIL/LQR/Roll/lqr_roll.png','-dpng','-r400');
