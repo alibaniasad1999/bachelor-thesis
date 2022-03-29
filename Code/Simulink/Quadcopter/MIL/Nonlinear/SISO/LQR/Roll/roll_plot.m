fdcce009dbb3f509ae8d01bfe11d74b5495d50d2
Ali <alibaniasad1999@yahoo.com>
2022-03-29 17:59:47 +0430

    fix linewith

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQR/Roll/roll_plot.m
@@ -3,12 +3,11 @@ weighting_matrix = XOpt;
 out = sim('Quad_Nonlinear_Roll_LQR_Model');
 
 subplot(2,1,1);
-plot(out.tout, out.roll_state(:, 1))
+plot(out.tout, out.roll_state(:, 1), 'linewidth', 2);
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
-
 subplot(2,1,2);
-plot(out.tout, out.roll_state(:, 2))
+plot(out.tout, out.roll_state(:, 2), 'linewidth', 2);
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
 
