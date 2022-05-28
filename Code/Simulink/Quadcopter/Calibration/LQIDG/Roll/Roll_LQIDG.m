27b125256aa19c599cd821b8595ffcb503fb7cac
Ali <alibaniasad1999@yahoo.com>
2022-05-28 17:24:58 +0430

    fix roll gain and add roll data

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll/Roll_LQIDG.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll/Roll_LQIDG.m
@@ -1,4 +1,8 @@
 clc; clear;
-load('11-Jan-2022-14-52-17-LQIDG_noise_ite1005.mat');
+load('11-Jan-2022-17-02-26-LQIDG_noise_ite1005.mat');
 weighting_matrix = XOpt;
+weighting_matrix(1) = -3.6903;
+weighting_matrix(2) =  -2.7960;
+weighting_matrix(3) = 0.5285;
+weighting_matrix(4) = -3.5806;
 OpenLoop_LQDG_riccati_solver_roll;
