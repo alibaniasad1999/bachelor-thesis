c6ee89bc66fb46f77a153230cb8ee36b53a25d79
alibaniasad1999 <alibaniasad1999@yahoo.com>
2022-06-11 00:59:49 +0430

    fix add fix

--- Code/Simulink/Quadcopter/RCP/OpenLoop/motor_less/Roll/parameter_estimation/result.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/motor_less/Roll/parameter_estimation/result.m
@@ -13,9 +13,9 @@ xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 % roll rate
 % subplot(2,1,2); 
-% plot(time, out.scenario_I(:, 2), 'r', 'linewidth', 2);
+% plot(time, out.scenario_I(:, 2), 'k--', 'linewidth', 2);
 % hold on;
-% plot(time, data_I(:, 2), 'k--', 'linewidth', 2);
+% plot(time, data_I(:, 2), 'r', 'linewidth', 2);
 % legend('Simulated', 'Measured');
 % xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 % ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
@@ -25,18 +25,18 @@ print('../../../../../../../../Figures/RCP/roll_ml_parameter_estimation/RCP_roll
 %% scenario II
 % subplot(2,1,1);
 % % roll
-plot(time, out.scenario_II(:, 1), 'r', 'linewidth', 2);
+plot(time, out.scenario_II(:, 1), 'k--', 'linewidth', 2);
 hold on;
-plot(time, data_II(:, 1), 'k--', 'linewidth', 2);
+plot(time, data_II(:, 1), 'r', 'linewidth', 2);
 hold off;
 legend('Simulated', 'Measured');
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 % % roll rate
 % subplot(2,1,2); 
-% plot(time, out.scenario_II(:, 2), 'r', 'linewidth', 2);
+% plot(time, out.scenario_II(:, 2), 'k--', 'linewidth', 2);
 % hold on;
-% plot(time, data_II(:, 2), 'k--', 'linewidth', 2);
+% plot(time, data_II(:, 2), 'r', 'linewidth', 2);
 % legend('Simulated', 'Measured');
 % xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 % ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
@@ -46,18 +46,18 @@ print('../../../../../../../../Figures/RCP/roll_ml_parameter_estimation/RCP_roll
 %% scenario III
 % subplot(2,1,1);
 % roll
-plot(time, out.scenario_III(:, 1), 'r', 'linewidth', 2);
+plot(time, out.scenario_III(:, 1), 'k--', 'linewidth', 2);
 hold on;
-plot(time, data_III(:, 1), 'k--', 'linewidth', 2);
+plot(time, data_III(:, 1), 'r', 'linewidth', 2);
 hold off;
 legend('Simulated', 'Measured');
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 % % roll rate
 % subplot(2,1,2); 
-% plot(time, out.scenario_III(:, 2), 'r', 'linewidth', 2);
+% plot(time, out.scenario_III(:, 2), 'k--', 'linewidth', 2);
 % hold on;
-% plot(time, data_III(:, 2), 'k--', 'linewidth', 2);
+% plot(time, data_III(:, 2), 'r', 'linewidth', 2);
 % legend('Simulated', 'Measured');
 % xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 % ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
@@ -67,18 +67,18 @@ print('../../../../../../../../Figures/RCP/roll_ml_parameter_estimation/RCP_roll
 %% scenario IV
 % subplot(2,1,1);
 % % roll
-plot(time, out.scenario_IV(:, 1), 'r', 'linewidth', 2);
+plot(time, out.scenario_IV(:, 1), 'k--', 'linewidth', 2);
 hold on;
-plot(time, data_IV(:, 1), 'k--', 'linewidth', 2);
+plot(time, data_IV(:, 1), 'r', 'linewidth', 2);
 hold off;
 legend('Simulated', 'Measured', 'Location','northwest');
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 % % roll rate
 % subplot(2,1,2); 
-% plot(time, out.scenario_IV(:, 2), 'r', 'linewidth', 2);
+% plot(time, out.scenario_IV(:, 2), 'k--', 'linewidth', 2);
 % hold on;
-% plot(time, data_IV(:, 2), 'k--', 'linewidth', 2);
+% plot(time, data_IV(:, 2), 'r', 'linewidth', 2);
 % legend('Simulated', 'Measured', 'Location','northwest');
 % xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 % ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
