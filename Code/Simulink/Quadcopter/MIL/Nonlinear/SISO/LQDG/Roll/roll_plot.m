a376e9665fa83fef1200f2ef0175e382f5d0d6f5
alibaniasad1999 <alibaniasad1999@yahoo.com>
2022-06-11 12:23:15 +0430

    add TCACS for no lag and add new fig for roll LQDG

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQDG/Roll/roll_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQDG/Roll/roll_plot.m
@@ -1,8 +1,8 @@
-load('29-Mar-2022-17-17-58-noise_ite_Roll_LQDG1000.mat');
+load('11-Jun-2022-12-19-13-noise_ite_Roll_LQDG500.mat');
 weighting_matrix = XOpt;
 out = sim('Quad_Nonlinear_Roll_Model_LQDG');
 
-subplot(2,1,1);
+% subplot(2,1,1);
 plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
 hold on;
 plot(out.tout, out.roll_state(:, 1), 'r', 'linewidth', 2);
@@ -10,13 +10,13 @@ hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 legend('Setpoint', 'LQDG Controller', 'Location','northeast');
-subplot(2,1,2);
-plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
-hold on
-plot(out.tout, out.roll_state(:, 2), 'r', 'linewidth', 2);
-hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
-ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
-legend('Setpoint', 'LQDG Controller', 'Location','northeast');
+% subplot(2,1,2);
+% plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
+% hold on
+% plot(out.tout, out.roll_state(:, 2), 'r', 'linewidth', 2);
+% hold off;
+% xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+% ylabel('roll rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
+% legend('Setpoint', 'LQDG Controller', 'Location','northeast');
 
 print('../../../../../../../../Figures/MIL/LQDG/Roll/lqdg_roll.png','-dpng','-r400');
