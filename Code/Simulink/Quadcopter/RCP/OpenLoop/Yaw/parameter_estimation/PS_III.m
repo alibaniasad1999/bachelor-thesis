a84a37aa280b7b8a0fa49bab29626ee06f468280
alibaniasad1999 <alibaniasad1999@yahoo.com>
2022-06-11 10:55:15 +0430

    add filter

--- Code/Simulink/Quadcopter/RCP/OpenLoop/Yaw/parameter_estimation/PS_III.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Yaw/parameter_estimation/PS_III.m
@@ -2,4 +2,5 @@ load('Scenario_III.mat', 'AHRS', 'AHRS_rate', 'Omega');
 time = 0:0.02:0.5;
 time = time';
 data = [AHRS(280:280+25,3), AHRS_rate(280:280+25,3)]*pi/180;
+data = lowpass(data, 50, 1e3);
 Omega_data = Omega(1, :);
