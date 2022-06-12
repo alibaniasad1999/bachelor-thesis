ae16a9a9eece291fb13f9c1208c7f2fefcdc1a05
alibaniasad1999 <alibaniasad1999@yahoo.com>
2022-06-12 20:40:15 +0430

    seperate roll and pitch and add omega fig

--- Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch/roll_pitch_plot.m
+++ Code/Simulink/Quadcopter/MIL/Nonlinear/SISO/LQIDG/Roll-Pitch/roll_pitch_plot.m
@@ -5,7 +5,7 @@ weighting_matrix = XOpt;
 weighting_matrix(7) = 0.6265; % change by me
 out = sim('Roll_Pitch_LQIDG');
 
-subplot(2,1,1);
+% subplot(2,1,1);
 plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
 hold on;
 plot(out.tout, out.roll_pitch_state(:, 1), 'r', 'linewidth', 2);
@@ -13,7 +13,8 @@ hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('roll$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 legend('Setpoint', 'LQIDG Controller', 'Location','southeast');
-subplot(2,1,2);
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll.png','-dpng','-r400');
+% subplot(2,1,2);
 plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
 hold on;
 plot(out.tout, out.roll_pitch_state(:, 2), 'r', 'linewidth', 2);
@@ -21,6 +22,7 @@ hold off;
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 ylabel('pitch$_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
 legend('Setpoint', 'LQIDG Controller', 'Location','southeast');
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_pitch.png','-dpng','-r400');
 % subplot(2,1,2);
 % plot(out.tout, zeros(1, length(out.tout)), '--k', 'linewidth', 2);
 % hold on
@@ -37,6 +39,21 @@ legend('Setpoint', 'LQIDG Controller', 'Location','southeast');
 % xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
 % ylabel('pitch rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
 % legend('Setpoint', 'LQIDG Controller', 'Location','southeast');
-
-
 print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll_pitch.png','-dpng','-r400');
+
+plot(out.tout, out.Omega(:, 1), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{1_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll_pitch_Omega_1.png','-dpng','-r400');
+plot(out.tout, out.Omega(:, 2), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{2_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll_pitch_Omega_2.png','-dpng','-r400');
+plot(out.tout, out.Omega(:, 3), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{3_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll_pitch_Omega_3.png','-dpng','-r400');
+plot(out.tout, out.Omega(:, 4), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{4_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../../../Figures/MIL/LQIDG/Roll_Pitch/lqidg_roll_pitch_Omega_4.png','-dpng','-r400');
