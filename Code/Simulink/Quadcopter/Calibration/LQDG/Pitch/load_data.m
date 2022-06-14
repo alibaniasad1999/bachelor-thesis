706c74326f23c026445bd3c1f10d9609d0ef935f
Ali <alibaniasad1999@yahoo.com>
2022-06-14 21:53:02 +0430

    change gain add to workspace and save data

--- Code/Simulink/Quadcopter/Calibration/LQDG/Pitch/load_data.m
+++ Code/Simulink/Quadcopter/Calibration/LQDG/Pitch/load_data.m
@@ -1,5 +1,5 @@
 % load data
 QuadConstants;
 % load('20-Dec-2021-01-16-27-noise_ite10005.mat');
-weighting_matrix =[12.5    4.6516   7.5730    3.9309    -1];
+weighting_matrix =[12.0    4.6516   7.5730    3.9309    -1];
 untitled2;
