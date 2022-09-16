8fb77282b947442fb390fe6af4a68ea66e13cfa3
Ali <alibaniasad1999@yahoo.com>
2022-09-16 14:18:12 +0430

    add new fig style

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/ploter.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll-Pitch-Yaw/ploter.m
@@ -11,11 +11,11 @@ plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time, 180/pi*simout(find(sim_time == 5):end, 1), '--k', 'linewidth', 2);
 hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\phi$(deg)', 'interpreter', 'latex', 'FontSize', 20);
 legend('Setpoint', 'LQIR-DGC', 'Location','northeast', 'FontSize', 20);
 axis([0 25 -0.5*180/pi 0.5*180/pi])
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\phi$(deg)', 'interpreter', 'latex', 'FontSize', 24);
 % % pitch rate q
 % subplot(2,1,2);
 % plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
@@ -31,63 +31,63 @@ plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time, 180/pi*simout(find(sim_time == 5):end, 2), '--k', 'linewidth', 2);
 hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\theta$(deg)', 'interpreter', 'latex', 'FontSize', 20);
-legend('Setpoint', 'LQIR-DGC', 'Location','northeast', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\theta$(deg)', 'interpreter', 'latex', 'FontSize', 24);
+legend('Setpoint', 'LQIR-DGC', 'Location','northeast', 'FontSize', 20);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_pitch.png','-dpng','-r400');
 
 plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time, 180/pi*simout(find(sim_time == 5):end, 3), '--k', 'linewidth', 2);
 hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\psi$(deg)', 'interpreter', 'latex', 'FontSize', 20);
-legend('Setpoint', 'LQIR-DGC', 'Location','northeast', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\psi$(deg)', 'interpreter', 'latex', 'FontSize', 24);
+legend('Setpoint', 'LQIR-DGC', 'Location','northeast', 'FontSize', 20);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_yaw.png','-dpng','-r400');
 
 
 plot(time, -180/pi*simout(find(sim_time == 5):end, 1), 'k', 'linewidth', 2);
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\phi_{(error)}$', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Error of Roll', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_roll_e.png','-dpng','-r400');
 
 
 plot(time, -180/pi*simout(find(sim_time == 5):end, 2), 'k', 'linewidth', 2);
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\theta_{(error)}$', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Error of Pitch', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_pitch_e.png','-dpng','-r400');
 
 plot(time, -180/pi*simout(find(sim_time == 5):end, 3), 'k', 'linewidth', 2);
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\psi_{(error)}$', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Error of Yaw', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_yaw_e.png','-dpng','-r400');
 
 
 plot(time, 60/2/pi*simout1(find(sim_time == 5):end, 1), 'k');
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\Omega_{1}$(RPM)', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\Omega_{1}$(RPM)', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_Omega_1.png','-dpng','-r400');
 
 plot(time, 60/2/pi*simout1(find(sim_time == 5):end, 2), 'k');
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\Omega_{2}$(RPM)', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\Omega_{2}$(RPM)', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_Omega_2.png','-dpng','-r400');
 
 plot(time, 60/2/pi*simout1(find(sim_time == 5):end, 3), 'k');
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\Omega_{3}$(RPM)', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\Omega_{3}$(RPM)', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_Omega_3.png','-dpng','-r400');
 
 plot(time, 60/2/pi*simout1(find(sim_time == 5):end, 4), 'k');
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 20);
-ylabel('$\Omega_{4}$(RPM)', 'interpreter', 'latex', 'FontSize', 20);
 set(gca, 'FontSize', 16)
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\Omega_{4}$(RPM)', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../Figures/Calibration/LQIDG/3DOF/lqidg_Omega_4.png','-dpng','-r400');
