8165bccbc5316568901d0f0e812655122cad5899
Ali <alibaniasad1999@yahoo.com>
2022-10-19 16:07:39 +0330

    add LQIDG controller

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/LQIDG_solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/LQIDG_solver.m
@@ -1,7 +1,18 @@
 % roll pitch LQIDG
 clc; clear;
-load('11-Jan-2022-15-55-56-LQIDG_noise_ite1005.mat');
-weighting_matrix = XOpt;
+% load('11-Jan-2022-15-55-56-LQIDG_noise_ite1005.mat');
+% weighting_matrix = XOpt;
+weighting_matrix = [
+    2.6385
+    2.4193
+    2.4193
+   -5.6310
+   -1.7615
+    1.8193
+    1.9193
+   -5.6310
+    0.1060
+    ];
 QuadConstants;
 OpenLoop_LQDG_riccati_solver_roll;
 OpenLoop_LQDG_riccati_solver_pitch;
--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/OpenLoop_LQDG_riccati_solver_pitch.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/OpenLoop_LQDG_riccati_solver_pitch.m
@@ -4,7 +4,7 @@ QuadConstants;
 global A B Q n S1 S2 R1_inv R2_inv
 
 A = [0  1 ;
-     B1 0];
+     0 0];
 % A for LQIDG
 A = [A      , zeros(2) ;
      eye(2) , zeros(2)];
@@ -19,7 +19,7 @@ q_v = [10^weighting_matrix(1) % pitch
 Q      = diag(q_v);
 R1     = eye(1);
 R1_inv = R1^-1;
-R2     = 10^weighting_matrix(5);
+R2     = 10^weighting_matrix(9);
 R2_inv = R2^-1;
 S1     = B* R1_inv * B';
 S2     = B* R2_inv * B';
@@ -44,7 +44,7 @@ p1 = p(1:n, :);
 p2 = p(n+1: end, :);
 
 % all player
-LQIDG_openloop_pitch = (R1_inv * B' * p1) ;
+LQIDG_openloop_pitch = (R1_inv * B' * p1)
 %% Functions %%
 function d = diff_eq_Riccati(~,p)
 global A Q n S1 S2
