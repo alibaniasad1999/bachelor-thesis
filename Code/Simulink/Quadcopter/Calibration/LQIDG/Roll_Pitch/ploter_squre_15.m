9f8dcedb23224d1061e2e87710e605edf7b7f2f1
Ali <alibaniasad1999@yahoo.com>
2022-11-19 22:15:37 +0330

    tune controller and add data with video

---
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/ploter_squre_15.m
@@ -0,0 +1,35 @@
+clc
+clear;
+
+load('data_film_squre_15.mat');
+time = 0:0.02:100;
+
+
+% square wave 
+freq=0.025;
+offset=0;
+amp=15;
+duty=50;
+
+sq_wav=offset+amp*square(2*pi*freq.*time,duty);
+sq_wav(end) = sq_wav(end-1);
+
+refrence = sq_wav;
+plot(time, refrence, 'r', 'linewidth', 2);
+hold on;
+plot(time, roll_pitch_LQIDG(:, 1)*180/pi, '--k', 'linewidth', 2);
+hold off;
+
+legend('Setpoint', 'LQIR-DG', 'Location','northeast', 'FontSize', 20);
+axis([0 100 -40 50])
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\phi$(deg)', 'interpreter', 'latex', 'FontSize', 24);
+print('../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Square/lqidg_roll.pdf','-desp');
+
+
+% refrence = sq_wav;
+% plot(time, -refrence, 'r', 'linewidth', 2);
+% hold on;
+% plot(time, roll_pitch_LQIDG(:, 2)*180/pi, '--k', 'linewidth', 2);
+% hold off;
