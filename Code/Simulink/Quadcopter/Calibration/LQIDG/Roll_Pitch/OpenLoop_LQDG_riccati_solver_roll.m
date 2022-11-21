71244f9f3d719dc4c9184af52d0582326e40a668
Ali <alibaniasad1999@yahoo.com>
2022-11-21 18:59:24 +0330

    add LQIDG and PID with LQIDG disturbance

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/OpenLoop_LQDG_riccati_solver_roll.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/OpenLoop_LQDG_riccati_solver_roll.m
@@ -44,7 +44,8 @@ p1 = p(1:n, :);
 p2 = p(n+1: end, :);
 
 % all player
-LQIDG_openloop_roll = (R1_inv * B' * p1)
+LQIDG_openloop_roll = (R1_inv * B' * p1);
+LQIDG_openloop_roll_d = (R2_inv * B' * p2);
 %% Functions %%
 function d = diff_eq_Riccati(~,p)
 global A Q n S1 S2
