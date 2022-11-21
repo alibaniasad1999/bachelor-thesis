28dfae68b67c1dca41cea2785753024a4b683881
Ali <alibaniasad1999@yahoo.com>
2022-11-21 15:59:31 +0330

    add disrurbance in roll and pitch

--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/Disturbance/LQIDG_solver.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/Disturbance/LQIDG_solver.m
@@ -3,13 +3,13 @@ clc; clear;
 % load('11-Jan-2022-15-55-56-LQIDG_noise_ite1005.mat');
 % weighting_matrix = XOpt;
 weighting_matrix = [
-   -1.7615
+    2.6085
     1.8193
     1.9193
    -5.6310
-    2.6385
-    2.4193
-    2.4193
+    1.7385
+    2.6193
+    2.6193
    -5.6310
     0.1060
     ];
--- Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/Disturbance/ploter.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDG/Roll_Pitch/Disturbance/ploter.m
@@ -2,7 +2,7 @@ clear;
 clc;
 %% plot data %%
 time = 0:0.02:95; % set time from start
-load('disturbance result.mat') % load data
+load('disturbance.mat') % load data
 sim_time = 0:0.02:100;
 % pitch
 % subplot(2,1,1);
@@ -10,20 +10,26 @@ plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time, roll_pitch_LQIDG_distubance(find(sim_time == 5):end, 1)*180/pi, '--k', 'linewidth', 2);
 hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
-ylabel('$\phi_{(deg)}$', 'interpreter', 'latex', 'FontSize', 18);
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\phi_{(deg)}$', 'interpreter', 'latex', 'FontSize', 24);
 legend('Setpoint', 'LQIDG Controller', 'Location','northeast');
 % axis([0 10 -1 0.2])
+xlim([0 80])
 % % pitch rate q
 % subplot(2,1,2);
-print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_roll.png','-dpng','-r400');
+% print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_roll.png','-dpng','-r400');
+print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_roll','-depsc');
+
 
 plot(time, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time, roll_pitch_LQIDG_distubance(find(sim_time == 5):end, 2)*180/pi, '--k', 'linewidth', 2);
 hold off;
-xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
-ylabel('$\theta_{(deg)}$', 'interpreter', 'latex', 'FontSize', 18);
+xlim([0 80])
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\theta_{(deg)}$', 'interpreter', 'latex', 'FontSize', 24);
 legend('Setpoint', 'LQIDG Controller', 'Location','northeast');
-print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_pitch.png','-dpng','-r400');
-
+% print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_pitch.png','-dpng','-r400');
+print('../../../../../../../Figures/Calibration/LQIDG/Roll_Pitch/Disturbance/lqidg_pitch','-depsc');
