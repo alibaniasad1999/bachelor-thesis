9f0e958ed524ffff32399c11ceef5962d114827f
Ali <alibaniasad1999@yahoo.com>
2022-12-14 20:01:56 +0330

    add data of LQR, LQIR and PID in 2D for compare

---
+++ Code/Simulink/Quadcopter/Calibration/LQR/roll-pitch/LQR_I_Solver.m
@@ -0,0 +1,15 @@
+% LQR-I solver
+QuadConstants;
+A = [0 1;
+    B1 0];
+B = [0; 1];
+C = eye(2);
+D = zeros(2, 1);
+load('21-Dec-2021-20-04-30-noise_ite_Pitch_LQR1005.mat')
+weighting_matrix = XOpt;
+Q1 = 10^(weighting_matrix(1));
+Q2 = 10^(weighting_matrix(2));
+R  = 10^weighting_matrix(3);
+I_gain = 10^(weighting_matrix(4));
+Q = diag([Q1, Q2]);
+[~, K, ~] = icare(A, B, Q, R);
---
+++ Code/Simulink/Quadcopter/Calibration/LQR/roll-pitch/ploter.m
@@ -0,0 +1,34 @@
+clear;
+clc;
+%% plot data %%
+time = 0:0.02:10; % set time from start
+load('pitch_LQR1.mat') % load data
+sim_time = 0:0.02:15;
+% pitch
+% subplot(2,1,1);
+plot(time, zeros(1, length(time)), '--k', 'linewidth', 2);
+hold on;
+plot(time, pitch_LQR.signals.values(find(sim_time == 5):end, 1), 'r', 'linewidth', 2);
+hold off;
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\theta_{(rad)}$', 'interpreter', 'latex', 'FontSize', 18);
+legend('Setpoint', 'LQR Controller', 'Location','northeast');
+% pitch rate q
+% subplot(2,1,2);
+% plot(time, zeros(1, length(time)), '--k', 'linewidth', 2);
+% hold on;
+% plot(time, pitch_LQIDG(find(sim_time == 5):end, 2), 'r', 'linewidth', 2);
+% hold off;
+% xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+% ylabel('pitch rate$_{(rad/\sec)}$', 'interpreter', 'latex', 'FontSize', 18);
+% legend('Setpoint', 'LQIDG Controller', 'Location','northeast');
+print('../../../../../../Figures/Calibration/LQR/Pitch/lqr_pitch.png','-dpng','-r400');
+plot(time, Omega.signals.values(find(sim_time == 5):end, 1), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{1_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../Figures/Calibration/LQR/Pitch/lqr_pitch_Omega_1.png','-dpng','-r400');
+
+plot(time, Omega.signals.values(find(sim_time == 5):end, 3), 'k');
+xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 16);
+ylabel('$\omega_{3_{(rad/\sec)}}$', 'interpreter', 'latex', 'FontSize', 18);
+print('../../../../../../Figures/Calibration/LQR/Pitch/lqr_pitch_Omega_3.png','-dpng','-r400');
