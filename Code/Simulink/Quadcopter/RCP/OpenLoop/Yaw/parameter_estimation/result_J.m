6ca579ba677ea2389dd5591b8b3256c77166c965
Ali <alibaniasad1999@yahoo.com>
2023-01-03 01:47:50 +0330

    add psi

--- Code/Simulink/Quadcopter/RCP/OpenLoop/Yaw/parameter_estimation/result_J.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Yaw/parameter_estimation/result_J.m
@@ -14,3 +14,19 @@ legend('Simulated', 'Measured', 'FontSize', 20);
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
 ylabel('$\psi_{(\textrm{deg})}$', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../../English_Journal/Figure/parameter_estimation/yaw/yaw','-depsc');
+%% trajectory %%
+load('Quadcopter_nonlinear_spesession.mat')
+C2_array = zeros(1, 4);
+C2_array(1) = C2;
+C2_array(2) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_1.Parameters.Value;
+C2_array(3) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_2.Parameters.Value;
+C2_array(4) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_3.Parameters.Value;
+
+
+
+plot(0:length(C2_array)-1, C2_array, '*k', 'markersize', 10)
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('Iterations', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Value of $\Gamma_7$', 'interpreter', 'latex', 'FontSize', 24);
+print('../../../../../../../English_Journal/Figure/parameter_estimation/yaw/yaw_parameter','-depsc');
+
