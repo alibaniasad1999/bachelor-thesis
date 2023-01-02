1fb9807f005c49c7559c7880f7fbbe581e7da0db
Ali <alibaniasad1999@yahoo.com>
2023-01-03 00:02:29 +0330

    add new fig of roll parameter trajectory and new word file

--- Code/Simulink/Quadcopter/RCP/OpenLoop/Roll/Parameter_estimation/QuadConstants.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Roll/Parameter_estimation/QuadConstants.m
@@ -2,7 +2,7 @@ QuadParameter;
 % A Constants
 A1 = (h_cg * g * m_tot) / (m_tot * h_cg^2 + J_xx);
 A2 = (2*m_tot * h_cg^2 + J_yy -J_zz) / (m_tot * h_cg^2 + J_xx);
-A3 = (b * d_cg) / (m_tot * h_cg^2 + J_xx);
+A3 = (b * d_cg) / (J_xx);
 A4 = (J_R) / (m_tot * h_cg^2 + J_xx);
 A5 = (m1 * g * miu_k * r_x) / (m_tot * h_cg^2 + J_xx);
 % B Constants
@@ -17,4 +17,4 @@ C2 = (d) / (J_zz);
 C3 = (m3 * g * miu_k * r_z) / (J_zz);
 % omega for linearzation 2000 RPM
 Omega = 2000 / 60 * 2 * pi;
-roll_parameter_estiation_result_I;
+% roll_parameter_estiation_result_I;
--- Code/Simulink/Quadcopter/RCP/OpenLoop/Roll/Parameter_estimation/result_J.m
+++ Code/Simulink/Quadcopter/RCP/OpenLoop/Roll/Parameter_estimation/result_J.m
@@ -13,5 +13,25 @@ legend('Simulated', 'Measured', 'FontSize', 20);
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
 ylabel('$\phi_{(\textrm{deg})}$', 'interpreter', 'latex', 'FontSize', 24);
 print('../../../../../../../English_Journal/Figure/parameter_estimation/roll/roll','-depsc');
+%% trajectory %%
+load('Quad_Nonlinear_Roll_PS_spesession.mat');
+A3_array = zeros(1, 10);
+A3_array(1) = A3;
+A3_array(2) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_1.Parameters.Value;
+A3_array(3) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_2.Parameters.Value;
+A3_array(4) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_3.Parameters.Value;
+A3_array(5) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_4.Parameters.Value;
+A3_array(6) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_5.Parameters.Value;
+A3_array(7) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_6.Parameters.Value;
+A3_array(8) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_7.Parameters.Value;
+A3_array(9) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_8.Parameters.Value;
+A3_array(10) = SDOSessionData.Data.Workspace.LocalWorkspace.EstimatedParams_9.Parameters.Value;
+
+
+plot(0:length(A3_array)-1, A3_array, '*k', 'markersize', 10)
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('Iterations', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Value of $\Gamma_1$', 'interpreter', 'latex', 'FontSize', 24);
+print('../../../../../../../English_Journal/Figure/parameter_estimation/roll/roll_parameter','-depsc');
 
 
