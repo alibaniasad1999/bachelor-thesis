e9fe2ae415d4925735de937f454f648052003631
Ali <alibaniasad1999@yahoo.com>
2023-02-24 15:28:24 +0330

    change color of compare fig to have difference in black and wight

--- Code/Simulink/Quadcopter/Calibration/LQIDGvsLQR/Roll_Pitch/ploter_J.m
+++ Code/Simulink/Quadcopter/Calibration/LQIDGvsLQR/Roll_Pitch/ploter_J.m
@@ -17,9 +17,9 @@ set(gca, 'FontSize', 16)
 plot(time-5, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time-5, 180/pi*(roll_pitch_LQIDG(:, 1)+0.0230), '--k', 'linewidth', 2);
-plot(time_new-5, 180/pi*LQIR_roll_pitch(:, 1), '--r', 'linewidth', 2);
-plot(time_new-5, -180/pi*roll_pitch_LQR.signals.values(:, 1), '--b', 'linewidth', 2);
-plot(time_new-5, -180/pi*roll_pitch_PID(:, 1), '--g', 'linewidth', 2);
+plot(time_new-5, 180/pi*LQIR_roll_pitch(:, 1), ':b', 'linewidth', 2);
+plot(time_new-5, -180/pi*roll_pitch_LQR.signals.values(:, 1), '-.', 'linewidth', 2);
+plot(time_new-5, -180/pi*roll_pitch_PID(:, 1), '+g', 'linewidth', .001);
 
 hold off;
 legend('Setpoint', 'LQIR-DG', 'LQIR', 'LQR', 'PID', 'Location','northeast', 'FontSize', 20);
@@ -27,6 +27,7 @@ axis([0 10 -20 30])
 set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
 xlabel('time($\sec)$', 'interpreter', 'latex', 'FontSize', 24);
 ylabel('$\phi$(deg)', 'interpreter', 'latex', 'FontSize', 24);
+%%
 print('../../../../../../English_Journal/Figure/implementation/lqidgvslqr/roll','-depsc');
 
 
@@ -36,9 +37,9 @@ set(gca, 'FontSize', 16)
 plot(time-5, zeros(1, length(time)), 'r', 'linewidth', 2);
 hold on;
 plot(time-5, 180/pi*roll_pitch_LQIDG(:, 2), '--k', 'linewidth', 2);
-plot(time_new-5, -180/pi*LQIR_roll_pitch(:, 2), '--r', 'linewidth', 2);
-plot(time_new-5, 180/pi*roll_pitch_LQR.signals.values(:, 2), '--b', 'linewidth', 2);
-plot(time_new-5, 180/pi*roll_pitch_PID(:, 2), '--g', 'linewidth', 2);
+plot(time_new-5, -180/pi*LQIR_roll_pitch(:, 2), ':b', 'linewidth', 2);
+plot(time_new-5, 180/pi*roll_pitch_LQR.signals.values(:, 2), '-.', 'linewidth', 2);
+plot(time_new-5, 180/pi*roll_pitch_PID(:, 2), '+g', 'linewidth', .02);
 hold off;
 axis([0 10 -60 60])
 legend('Setpoint', 'LQIR-DG', 'LQIR', 'LQR', 'PID', 'Location','northeast', 'FontSize', 20);
